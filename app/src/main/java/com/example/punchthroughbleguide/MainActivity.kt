6fc109423a8dee06c6101b4222ccf42f0ef81afb
tomas <tomasaronjonsson@gmail.com>
2023-04-04 12:11:55 +0000

    first commit

---
+++ app/src/main/java/com/example/punchthroughbleguide/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.punchthroughbleguide
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+
+
+class MainActivity : AppCompatActivity() {
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!bluetoothAdapter.isEnabled) {
+            promptEnableBluetooth()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun promptEnableBluetooth() {
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
+        }
+    }
+}
